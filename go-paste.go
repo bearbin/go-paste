aec66353a26734888a98ef476d43fea356b605b8
Alessio Treglia <alessio@tendermint.com>
2020-11-14 14:48:35 +0000

    upgrade to urfave/cli/v2

--- go-paste.go
+++ go-paste.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"errors"
-	"github.com/bearbin/go-paste/fpaste"
-	"github.com/bearbin/go-paste/pastebin"
-	"github.com/codegangsta/cli"
+	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/bearbin/go-paste/fpaste"
+	"github.com/bearbin/go-paste/pastebin"
 )
 
 var errUnknownService = errors.New("unknown paste service")
@@ -15,54 +18,57 @@ func main() {
 	app := cli.NewApp()
 	app.Usage = "get and put pastes from pastebin and other paste sites."
 	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "service, s", Value: "pastebin", Usage: "the pastebin service to use"},
+		&cli.StringFlag{Name: "service, s", Value: "pastebin", Usage: "the pastebin service to use"},
 	}
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		{
-			Name:      "put",
-			ShortName: "p",
-			Usage:     "put a paste",
+			Name:    "put",
+			Usage:   "put a paste",
+			Aliases: []string{"p"},
 			Flags: []cli.Flag{
-				cli.BoolFlag{Name: "id", Usage: "return the paste id not the url"},
-				cli.StringFlag{Name: "title, t", Value: "", Usage: "the title for the paste"},
+				&cli.BoolFlag{Name: "id", Usage: "return the paste id not the url"},
+				&cli.StringFlag{Name: "title, t", Value: "", Usage: "the title for the paste"},
 			},
-			Action: func(c *cli.Context) {
-				srv, err := convertService(c.GlobalString("service"))
+			Action: func(c *cli.Context) error {
+				srv, err := convertService(c.String("service"))
 				if err != nil {
-					println("ERROR:", err.Error())
-					os.Exit(1)
+					return err
 				}
+
 				var text []byte
 				if c.Args().First() == "-" || c.Args().First() == "" {
 					text, err = ioutil.ReadAll(os.Stdin)
 				} else {
 					text, err = ioutil.ReadFile(c.Args().First())
 				}
+
 				if err != nil {
-					println("ERROR:", err.Error())
-					os.Exit(1)
+					return err
 				}
+
 				code, err := srv.Put(string(text), c.String("title"))
 				if err != nil {
-					println("ERROR:", err.Error())
-					os.Exit(1)
+					return err
 				}
-				if c.Bool("id") {
-					println(code)
-				} else {
-					println(srv.WrapID(code))
+
+				if !c.Bool("id") {
+					code = srv.WrapID(code)
 				}
+
+				_, _ = fmt.Fprintln(app.Writer, code)
+
+				return nil
 			},
 		},
 		{
-			Name:      "get",
-			ShortName: "g",
-			Usage:     "get a paste from its url",
+			Name:    "get",
+			Usage:   "get a paste from its url",
+			Aliases: []string{"g"},
 			Flags: []cli.Flag{
-				cli.BoolFlag{Name: "id", Usage: "get a paste from its ID instead of its URL"},
+				&cli.BoolFlag{Name: "id", Usage: "get a paste from its ID instead of its URL"},
 			},
-			Action: func(c *cli.Context) {
-				srv, err := convertService(c.GlobalString("service"))
+			Action: func(c *cli.Context) error {
+				srv, err := convertService(c.String("service"))
 				if err != nil {
 					println("ERROR:", err.Error())
 					os.Exit(1)
@@ -75,14 +81,21 @@ func main() {
 				}
 				text, err := srv.Get(id)
 				if err != nil {
-					println("ERROR:", err.Error())
-					os.Exit(1)
+					return err
 				}
-				println(text)
+
+				_, _ = fmt.Fprintln(app.Writer, text)
+
+				return nil
 			},
 		},
 	}
-	app.Run(os.Args)
+
+	if err := app.Run(os.Args); err != nil {
+		_, _ = fmt.Fprintf(app.ErrWriter, "%v\n", err)
+
+		os.Exit(1)
+	}
 }
 
 func convertService(srv string) (service, error) {
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bearbin/go-paste
+
+go 1.15
+
+require github.com/urfave/cli/v2 v2.3.0
